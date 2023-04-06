076c58002e235dd1724a82e77f1ad0ebc0b41cab
Yottaxx <605857866@qq.com>
2023-04-06 21:55:37 +0800

    update

--- run_scimrc.py
+++ run_scimrc.py
@@ -46,7 +46,6 @@ from transformers.file_utils import is_offline_mode
 from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version
 from transformers.utils.versions import require_version
-from BLEU import Bleu
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 
